46fed0cdcd95a4f87938ada907fa0e7d15c90e13
Kateryna Artemieva <moruga.katya@gmail.com>
2023-06-01 16:11:50 +0100

    task-2-done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,36 @@
-export const App = () => {
-  return (
-    <>
-    </>
-  );
-};
+import { Component } from 'react';
+import initialContacs from '../contacts.json';
+import { Box } from './Box/Box.styled';
+import { PhoneBook } from './PhoneBook/PhoneBook.styled';
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactsList } from './ContactsList/ContactsList';
+
+export class App extends Component {
+  state = { contacts: initialContacs };
+
+  addContact = newContact => {
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, newContact],
+    }));
+  };
+
+  deleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
+
+  render() {
+    return (
+      <Box>
+        <PhoneBook>
+          <ContactForm addContact={this.addContact} />
+          <ContactsList
+            items={this.state.contacts}
+            deleteContact={this.deleteContact}
+          />
+        </PhoneBook>
+      </Box>
+    );
+  }
+}
