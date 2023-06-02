24e946c6b985697993e3dc0468ba8b98dcf0a18f
Kateryna Artemieva <moruga.katya@gmail.com>
2023-06-02 15:21:30 +0100

    fix-homework

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,15 +3,25 @@ import initialContacs from '../contacts.json';
 import { Box } from './Box/Box.styled';
 import { PhoneBook } from './PhoneBook/PhoneBook.styled';
 import { ContactForm } from './ContactForm/ContactForm';
+import { Filter } from './Filter/Filter';
 import { ContactsList } from './ContactsList/ContactsList';
 
 export class App extends Component {
-  state = { contacts: initialContacs };
+  state = { contacts: initialContacs, filter: '' };
 
   addContact = newContact => {
-    this.setState(prevState => ({
-      contacts: [...prevState.contacts, newContact],
-    }));
+    const { name, number } = newContact;
+    const { contacts } = this.state;
+    if (
+      contacts.some(contact => contact.name === name) ||
+      contacts.some(contact => contact.number === number)
+    ) {
+      alert(`This one is already in contacts`);
+    } else {
+      this.setState(prevState => ({
+        contacts: [...prevState.contacts, newContact],
+      }));
+    }
   };
 
   deleteContact = contactId => {
@@ -20,13 +30,27 @@ export class App extends Component {
     }));
   };
 
+  changeFilter = e => {
+    this.setState({ filter: e.target.value });
+  };
+
+  getFilteredContacts = () => {
+    const { filter, contacts } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
   render() {
     return (
       <Box>
         <PhoneBook>
           <ContactForm addContact={this.addContact} />
+          <Filter value={this.state.value} onChange={this.changeFilter} />
           <ContactsList
-            items={this.state.contacts}
+            items={this.getFilteredContacts()}
             deleteContact={this.deleteContact}
           />
         </PhoneBook>
